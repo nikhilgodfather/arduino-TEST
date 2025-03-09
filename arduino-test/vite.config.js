3d972b2244b214828e9c4c5f67eb928af4d36548
Nikhil Agarwal <153534255+nikhilgodfather@users.noreply.github.com>
2025-03-09 11:03:59 +0530

    Update vite.config.js

--- arduino-test/vite.config.js
+++ arduino-test/vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   server: {
     host: "0.0.0.0", // Allow all network interfaces
     port: process.env.PORT || 3000, // Use Render's PORT environment variable
-    strictPort: true, // Ensure Vite fails if the port is not available
+    strictPort: true,
+    allowedHosts: ["arduino-test.onrender.com"],  // Ensure Vite fails if the port is not available
   },
   preview: {
     port: process.env.PORT || 3000, // Ensures the preview uses the right port
